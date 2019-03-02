30018a7730dea50a90f862c074aab41b4d11c39a
Micah Mount <micah.mount@gmail.com>
2019-03-01 23:36:46 -0800

    Update to guessing game.

--- 1-HTML-CSS-JavaScript/js_guessing_game.js
+++ 1-HTML-CSS-JavaScript/js_guessing_game.js
@@ -5,12 +5,12 @@ var finished = false;
 var guesses = 0;
 
 function do_game(){
-    var random_number = Math.random() * 100;
+    var random_number = Math.random() * 40;
     var random_number_integer = Math.floor(random_number);
-    target = random_number_integer + 1;
+    target = random_number_integer + 10;
 
     while (!finished){
-        guess_input_text = prompt("I'm thinking of a number " + "in the range of 1 to 100.\n\n" + "What is the number?");
+        guess_input_text = prompt("I'm thinking of a number " + "in the range of 10 to 50.\n\n" + "What is the number?");
         guess_input = parseInt(guess_input_text);
         guesses += 1;
         finished = check_guess();
@@ -19,11 +19,11 @@ function do_game(){
 
 function check_guess(){
     if(isNaN(guess_input)){
-        alert("You have not entered a number. \n\n" + "Please enter a number in the range 1 to 100.");
+        alert("You have not entered a number. \n\n" + "Please enter a number in the range 10 to 50.");
         return false;
     }
-    if ((guess_input < 1) || (guess_input > 100)) {
-        alert("That's a number; but it's not between 1 and 100.\n\nPlease enter an integer number in the range 1 to 100.");
+    if ((guess_input < 10) || (guess_input > 50)) {
+        alert("That's a number; but it's not between 10 and 50.\n\nPlease enter an integer number in the range 10 to 50.");
         return false;
     }
     if (guess_input > target) {
