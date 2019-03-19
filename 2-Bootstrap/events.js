a9a20bf27a79d58577943ee6d8d0a1195657be47
Micah Mount <micah.mount@gmail.com>
2019-03-19 01:04:41 -0700

    Adds the events js exercise.

---
+++ 2-Bootstrap/events.js
@@ -0,0 +1,14 @@
+var a = 0;
+var b = 0; 
+var c = 0; 
+
+function changeBackground() {
+    var x = document.getElementById('bg');
+    bg.style.backgroundColor = "rgb(" + a + ", " + b + ", " + c + ")";
+    a += 1;
+    b += a + 1;
+    c += b + 1;
+    if (a > 255) a = a - b;
+    if (b > 255) b = a;
+    if (c > 255) c = b;
+}
