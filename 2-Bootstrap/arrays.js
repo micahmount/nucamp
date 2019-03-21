df9f901451015d865e22d0ca081e45e46ede1753
Micah Mount <micah.mount@gmail.com>
2019-03-21 15:24:50 -0700

    Updates arrays.js per help from teachers.

--- 2-Bootstrap/arrays.js
+++ 2-Bootstrap/arrays.js
@@ -1,8 +1,9 @@
 var myArray = ["Red", "Green", "White", "Black"];
-var myString = " ";
-for (i = 0; i <= myArray.length; i++) {
-    myString += myArray[i] + ", "; //https://stackoverflow.com/questions/39584129/concatenate-string-through-for-loop
-    console.log(myString);
-    //console.log(myArray.join(", "));
-    //console.log(myArray.join("+"));
+var myString = "";
+for (i = 0; i < myArray.length; i++) {
+    myString += myArray[i];
+    if (i < myArray.length -1){
+        myString += ", ";  //https://stackoverflow.com/questions/39584129/concatenate-string-through-for-loop
+     }
 }
+console.log(myString);
