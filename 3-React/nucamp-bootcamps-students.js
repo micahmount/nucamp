c32de4ed113493facf9ad8d8ff5cc0e8c0572c13
Micah Mount <micah.mount@gmail.com>
2019-04-24 12:40:14 -0700

    Wk0 in-person assignment(not in person this week), Task 1.

---
+++ 3-React/nucamp-bootcamps-students.js
@@ -0,0 +1,32 @@
+const students = [
+    [
+        "Neo",
+        "neo@matrix.com",
+        "Tacoma",
+    ],
+    [
+        "Morpheus",
+        "morpheus@matrix.com",
+        "Tacoma",
+    ],
+    [
+        "Trinity",
+        "trinity@matrix.com",
+        "Spokane",
+    ],
+    [
+        "Cypher",
+        "cypher@matrix.com",
+        "Seattle",
+    ],
+    [
+        "The Oracle",
+        "oracle@matrix.com",
+        "Marysville",
+    ],
+    [
+        "Agent Smith",
+        "smith@matrix.com",
+        "Bellingham",
+    ]
+]
