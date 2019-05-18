941ad67acf41c185fe2f4facec3ea1819073192f
Micah Mount <micah.mount@gmail.com>
2019-05-18 12:24:34 -0700

    Wk 3 In Person assignment.

--- 3-React/confusion/src/components/DishdetailComponent.js
+++ 3-React/confusion/src/components/DishdetailComponent.js
@@ -1,6 +1,100 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, Button, Col, Row, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => (val) && (val.length >= len);
+
+class CommentForm extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isModalOpen: false
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  }
+
+  handleSubmit(values) {
+    console.log("Currnet state is: " + JSON.stringify(values));
+    alert("Currnet state is: " + JSON.stringify(values));
+  }
+
+
+  render() {
+    return (
+      // BUTTON
+      <React.Fragment>
+        <Button onClick={this.toggleModal} outline color="secondary">
+          <span className="fa fa-lg fa-pencil"></span> Submit Comment
+        </Button>
+        {/* Modal */}
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+          <ModalBody>
+            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+              <Row className="form-group">
+                <Col>
+                  <Label htmlFor="rating">Rating</Label>
+                  <Control.select model=".rating" name="rating" className="form-control">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </Control.select>
+                </Col>
+              </Row>
+              <Row className="form-group">
+                <Col>
+                <Label htmlFor="name">Your Name</Label>
+                  <Control.text model=".name" id="name" name="name"
+                    placeholder="Your Name"
+                    className="form-control"
+                    validators={{
+                      required, minLength: minLength(3), maxLength: maxLength(15)
+                    }}
+                  />
+                  <Errors
+                    className="text-danger"
+                    model=".name"
+                    show="touched"
+                    messages={{
+                      required: 'Required',
+                      minLength: "Must be greater than 2 characters",
+                      maxLength: "Must be 15 characters or less"
+                    }}
+                  />
+                </Col>
+              </Row>
+              <Row className="form-group">
+                <Col>
+                  <Label htmlFor="comment">Comment</Label>
+                  <Control.textarea model=".comment" id="comment" name="comment" rows="6" className="form-control" />
+                </Col>
+              </Row>
+              <Button type="submit" color="primary">
+                Submit
+              </Button>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+      </React.Fragment>
+      
+
+    );
+  }
+}
 
 function RenderDish({dish}) {
     return (
@@ -30,6 +124,9 @@ function RenderComments({comments}) {
             );
         })}
         </ul>
+        <div>
+          <CommentForm />
+        </div>
         </div>
     ); 
     else
@@ -63,4 +160,5 @@ const DishDetail = (props) => {
             <div></div>
             );
 }
+
 export default DishDetail;
