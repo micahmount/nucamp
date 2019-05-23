d672dd048ad8efc882873d57561b699c1aba8b0e
Micah Mount <micah.mount@gmail.com>
2019-05-23 01:03:59 -0700

    Redux Thunk

--- 3-React/confusion/src/redux/configureStore.js
+++ 3-React/confusion/src/redux/configureStore.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Dishes } from './reducer-dishes';
 import { Comments } from './reducer-comments';
 import { Leaders } from './reducer-leaders';
 import { Promotions } from './reducer-promotions';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -11,7 +13,8 @@ export const ConfigureStore = () => {
             comments: Comments,
             leaders: Leaders,
             promotions: Promotions
-        })
+        }),
+        applyMiddleware(thunk, logger)
     );
 
     return store;
