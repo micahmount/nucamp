2628024d191f1a99fc18063ef31e1cd1d0c103d8
Micah Mount <micah.mount@gmail.com>
2019-06-19 01:26:46 -0700

    React Native - Wk3 - Alerts

--- 4-React-Native/confusion/components/FavoriteComponent.js
+++ 4-React-Native/confusion/components/FavoriteComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import { FlatList, View, Text } from 'react-native';
+import { FlatList, View, Text, Alert } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import Swipeout from 'react-native-swipeout';
+import { deleteFavorite, commentsFailed } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
@@ -11,6 +13,10 @@ const mapStateToProps = state => {
         favorites: state.favorites,
     }
 }
+
+const mapDispatchToProps = dispatch => ({
+    deleteFavorite: (dishId) => dispatch(deleteFavorite(dishId))
+});
 
 class Favorites extends Component {
     
@@ -22,14 +28,42 @@ class Favorites extends Component {
         const { navigate } = this.props.navigation;
 
         const renderMenuItem = ({item, index}) => {
+
+            const rightButton = [
+                {
+                    text: 'Delete',
+                    type: 'delete',
+                    onPress: () => {
+                        Alert.alert(
+                            'Delete Favorite?',
+                            'Are you sure you wish to delete the favorite dish ' + item.name + '?',
+                            [
+                                {
+                                    text: 'Cancel',
+                                    onPress: () => console.log(item.name + + 'Not Deleted'),
+                                    style: ' cancel'
+                                },
+                                {
+                                    text: 'OK',
+                                    onPress: () => this.props.deleteFavorite(item.id)
+                                }
+                            ],
+                            { cancelable: false }
+                        );
+                    }
+                }
+            ];
+
             return (
-                <ListItem
-                    key={index}
-                    title={item.name}
-                    subtitle={item.description}
-                    onPress={() => navigate('DishDetail', {dishId: item.id})}
-                    leftAvatar={{ source: {uri: baseUrl + item.image}}}
-                />
+                <Swipeout right={rightButton} autoClose={true}>
+                    <ListItem
+                        key={index}
+                        title={item.name}
+                        subtitle={item.description}
+                        onPress={() => navigate('DishDetail', { dishId: item.id })}
+                        leftAvatar={{ source: { uri: baseUrl + item.image } }}
+                    />
+                </Swipeout>
             );
         };
 
@@ -57,4 +91,4 @@ class Favorites extends Component {
     }
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
