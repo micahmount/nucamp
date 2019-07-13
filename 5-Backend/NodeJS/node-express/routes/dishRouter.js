7c48f1288baf34fb08c3f9352e26f6aa12871ac7
Micah Mount <micah.mount@gmail.com>
2019-07-13 11:24:26 -0700

    Backend - Wk 1 Assignment

--- 5-Backend/NodeJS/node-express/routes/dishRouter.js
+++ 5-Backend/NodeJS/node-express/routes/dishRouter.js
@@ -32,18 +32,18 @@ dishRouter.route('/:dishID')
         next();
     })
     .get((req, res, next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+        res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
     })
     .post((req, res, next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on /dishes/' + req.params.dishId);
+        res.statusCode = 403;
+        res.end('POST operation not supported on /dishes/' + req.params.dishId);
     })
     .put((req, res, next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name +
-        ' with details: ' + req.body.description);
+        res.write('Updating the dish: ' + req.params.dishId + '\n');
+        res.end('Will update the dish: ' + req.body.name +
+            ' with details: ' + req.body.description);
     })
     .delete((req, res, next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
+        res.end('Deleting dish: ' + req.params.dishId);
     });
 module.exports = dishRouter;
