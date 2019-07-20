be7e020bdd5e8f64edc4eca9467225ac95fc0c81
Micah Mount <micah.mount@gmail.com>
2019-07-19 22:06:32 -0700

    Mongoose Part 2

--- 5-Backend/NodeJS/node-mongoose/index.js
+++ 5-Backend/NodeJS/node-mongoose/index.js
@@ -8,12 +8,10 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log("Connected correctly to database");
 
-    const newDish = Dishes({
+    Dishes.create({
         name: 'Uthapizza',
         description: 'test'
-    });
-
-    newDish.save()
+    })
         .then((dish) => {
             console.log(dish);
 
